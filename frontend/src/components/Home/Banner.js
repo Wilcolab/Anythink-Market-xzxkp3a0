0a11538915e2a279191b2502adb988476f839932
Oscar Norambuena <58641787+muteki85@users.noreply.github.com>
2023-02-24 20:17:36 +0000

    Refactoring banner component

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,19 +1,30 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
 
+const StyledBanner = styled.div `
+  background: ${
+    props => props.theme.dark
+};
+`;
+
 const Banner = () => {
-  return (
-    <div className="banner text-white">
-      <div className="container p-4 text-center">
-        <img src={logo} alt="banner" />
-        <div>
-          <span>A place to </span>
-          <span id="get-part">get</span>
-          <span> the cool stuff.</span>
-        </div>
-      </div>
-    </div>
-  );
+    return (
+        <StyledBanner>
+            <div className="banner text-white">
+                <div className="container p-4 text-center">
+                    <img src={logo}
+                        alt="banner"/>
+                    <div>
+                        <span>A place to
+                        </span>
+                        <span id="get-part">get</span>
+                        <span>
+                            the cool stuff.</span>
+                    </div>
+                </div>
+            </div>
+        </StyledBanner>
+    );
 };
 
 export default Banner;
