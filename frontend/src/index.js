0afa36c112c8a4c1fe4b7f83b241a15c677dbc2b
Oscar Norambuena <58641787+muteki85@users.noreply.github.com>
2023-02-24 20:22:49 +0000

    Adding Globalstyles

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,26 +1,29 @@
 import "./custom.scss";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
+import {Provider} from "react-redux";
 import React from "react";
-import { store, history } from "./store";
+import {store, history} from "./store";
 
-import { Route, Switch } from "react-router-dom";
-import { ConnectedRouter } from "react-router-redux";
+import {Route, Switch} from "react-router-dom";
+import {ConnectedRouter} from "react-router-redux";
 
 import App from "./components/App";
-import { ThemeProvider } from "styled-components";
+import {ThemeProvider} from "styled-components";
 import theme from './styles/theme';
+import GlobalStyle from './styles/GlobalStyle';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <ThemeProvider theme={theme}>
-        <Switch>
-          <Route path="/" component={App} />
-        </Switch>
-      </ThemeProvider>
-    </ConnectedRouter>
-  </Provider>,
+ReactDOM.render (
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <ThemeProvider theme={theme}>
+                <GlobalStyle/>
+                <Switch>
+                    <Route path="/"
+                        component={App}/>
+                </Switch>
+            </ThemeProvider>
+        </ConnectedRouter>
+    </Provider>,
 
-  document.getElementById("root")
+    document.getElementById("root")
 );
