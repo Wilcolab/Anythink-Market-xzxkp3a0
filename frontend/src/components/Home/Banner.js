6b9d15ba994704941b6c9cb6708ed62c18d40257
WilcoApp <hello@wilco.gg>
2023-02-24 20:50:10 +0000

    Let it sink

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,31 +1,19 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
-import styled from "styled-components";
-
-const StyledBanner = styled.div `
-  background: ${
-    props => props.theme.dark
-};
-`;
 
 const Banner = () => {
-    return (
-        <StyledBanner>
-            <div className="banner text-white">
-                <div className="container p-4 text-center">
-                    <img src={logo}
-                        alt="banner"/>
-                    <div>
-                        <span>A place to
-                        </span>
-                        <span id="get-part">get</span>
-                        <span>
-                            the cool stuff.</span>
-                    </div>
-                </div>
-            </div>
-        </StyledBanner>
-    );
+  return (
+    <div className="banner text-white">
+      <div className="container p-4 text-center">
+        <img src={logo} alt="banner" />
+        <div>
+          <span>A place to </span>
+          <span id="get-part">get</span>
+          <span> the cool stuff.</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
